b23c3876fdc178b494eb13dd02771bea72d5ee48
Chris Ridgers <studentjester42@gmail.com>
2015-12-06 22:21:46 +0000

    Added homebrew installation
    Added homebrew installation via https and curl.  Added further info on
    condition checks for homebrew and dev cli tools.

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,5 +8,16 @@ if [[ $OSTYPE =~ ^darwin ]]; then
 	if [[ $(xcode-select -p > /dev/null 2>&1) ]]; then
 		echo "Info    | Install    | xcode"
 		xcode-select --install
+	else
+		echo "Info    | Already Installed    | xcode"
+	fi
+
+	# Download and install homebrew
+	if [[ ! -x /usr/local/bin/brew ]]; then
+		echo "Info    | Install    | Homebrew"
+		ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+		export PATH=/usr/local/bin:$PATH
+	else
+		echo "Info    | Already Installed    | Homebrew"
 	fi
 fi
